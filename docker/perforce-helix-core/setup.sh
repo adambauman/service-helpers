e0a9d48403fbfb0e679ded62fa5c24cd8cda2a55
Adam J. Bauman <adam@kungfutreachery.net>
2024-01-13 22:33:35 -0600

    Add configuration call

--- docker/perforce-helix-core/setup.sh
+++ docker/perforce-helix-core/setup.sh
@@ -62,5 +62,25 @@ if [ 0 -ne $? ]; then
     exit
 fi
 
+
+cd "${docker_directory}"
+#$as_sudo docker compose up -d
+#if [ 0 -ne $? ]; then
+#    echo "Error starting docker."
+#    exit
+#fi
+
 echo ""
-echo "Success!"
+echo "Running the Perforce Helix setup script. Use these parameters when prompted:"
+echo "- Server name: master"
+echo "- Server root: /perforce-data"
+echo "- Server port: 1666"
+echo "- Enter a very strong password for the super user"
+echo ""
+$as_sudo docker compose run --rm perforce /opt/perforce/sbin/configure-helix-p4d.sh
+
+echo ""
+echo "Success! Run \"sudo docker compose up -d\" when you're ready to start the service!"
+echo ""
+
+exit
