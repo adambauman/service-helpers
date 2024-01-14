bfd52ce9f559caed88cd3e440896dd3860d32110
Adam J. Bauman <adam@kungfutreachery.net>
2024-01-14 12:58:29 -0600

    Yes switches

--- docker/install-docker.sh
+++ docker/install-docker.sh
@@ -11,7 +11,7 @@ echo "Starting docker install..."
 
 # Add Docker's official GPG key:
 $as_sudo apt-get update
-$as_sudo apt-get install ca-certificates curl gnupg
+$as_sudo apt-get install ca-certificates curl gnupg --yes
 $as_sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | $as_sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 $as_sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -21,9 +21,9 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   $as_sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-$as_sudo apt-get update
+$as_sudo apt-get update --yes
 
-$as_sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+$as_sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin --yes
 
 echo ""
 echo "Success!"
